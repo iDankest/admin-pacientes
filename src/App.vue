09e9bb051ac15029bf6c447603597a13cdc99bee
Kilian <dankestmirror@hotmail.es>
2025-06-05 20:29:48 +0100

    $emits pasar info al padre app

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,37 @@
 <script setup>
+import {ref, reactive} from 'vue'
   import Header from "./components/Header.vue";
   import Formulario from "./components/Formulario.vue";
+
+const pacientes = ref([])
+
+const paciente = reactive({
+    nombre: '',
+    email: '',
+    alta: '',
+    sintomas: '',
+})
+
 </script>
 
 <template>
   <div class="container mx-auto mt-20">
     <Header />
     <div class="mt-12 md:flex md:justify-between">
-      <Formulario />
+      <Formulario 
+      v-model:nombre="paciente.nombre"
+      v-model:email="paciente.email"
+      v-model:alta="paciente.alta"
+      v-model:sintomas="paciente.sintomas"
+      />
+
+      <div class="md:w-1/2 md:h-screen overflow-y-scroll">
+        <h3 class="font-black text-3xl text-center">Administra tus pacientes</h3>
+        <div v-if="pacientes.length > 0">
+
+        </div>
+        <p v-else class="mt-10 text-2xl text-center">No hay pacientes</p>
+      </div>
     </div>
   </div>
 </template>
--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -7,11 +7,25 @@ const alerta= reactive({
     mensaje: ''
 })
 
-const paciente = reactive({
-    nombre: '',
-    email: '',
-    alta: '',
-    sintomas: '',
+defineEmits(['update:nombre', 'update:email', 'update:alta', 'update:sintomas'])//Emits para pasar info al padre
+
+const props = defineProps({ //Props para recibir info del padre mostrar la info en el Dom
+    nombre:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true
+    },
+    alta:{
+        type: String,
+        required: true
+    },
+    sintomas:{
+        type: String,
+        required: true
+    }
 })
 
 const validando = e => {
@@ -35,32 +49,32 @@ const validando = e => {
             v-if="alerta.mensaje"
             :alerta="alerta"
             />
+            {{ nombre }}
             <form class="shadow-md bg-white rounded-lg py-10 px-5 mb-10" @submit.prevent="validando">
                 <p id="error" class="text-red-500 mt-5"></p>
                 <div class="mb-5">
-                    {{ paciente.nombre }}
                     <label for="nombre" class="block text-gray-700 uppercase font-bold">
                         Nombre del paciente
                     </label>
-                    <input type="text" v-model="paciente.nombre"  id="nombre" placeholder="Nombre del paciente" class="border-2 placeholder-gray-400 w-full p-2 mt-2 rounded-md" >
+                    <input type="text" @input="$emit('update:nombre', $event.target.value)" id="nombre" placeholder="Nombre del paciente" class="border-2 placeholder-gray-400 w-full p-2 mt-2 rounded-md" >
                 </div>
                 <div class="mb-5">
                     <label for="email" class="block text-gray-700 uppercase font-bold">
                         Email
                     </label>
-                    <input type="email" v-model="paciente.email" id="email" placeholder="Email del paciente" class="border-2 placeholder-gray-400 w-full p-2 mt-2 rounded-md">
+                    <input type="email"  id="email" placeholder="Email del paciente" class="border-2 placeholder-gray-400 w-full p-2 mt-2 rounded-md">
                 </div>
                 <div class="mb-5">
                     <label for="alta" class="block text-gray-700 uppercase font-bold">
                         Alta
                     </label>
-                    <input type="date" v-model="paciente.alta" id="alta" placeholder="Alta del paciente" class="border-2 placeholder-gray-400 w-full p-2 mt-2 rounded-md">
+                    <input type="date"  id="alta" placeholder="Alta del paciente" class="border-2 placeholder-gray-400 w-full p-2 mt-2 rounded-md">
                 </div>
                 <div class="mb-5">
                     <label for="sintomas" class="block text-gray-700 uppercase font-bold">
                         Síntomas
                     </label>
-                    <textarea id="sintomas" v-model="paciente.sintomas" placeholder="Síntomas del paciente" class="border-2 placeholder-gray-400 w-full p-2 mt-2 rounded-md h-40"></textarea>
+                    <textarea id="sintomas" placeholder="Síntomas del paciente" class="border-2 placeholder-gray-400 w-full p-2 mt-2 rounded-md h-40"></textarea>
                 </div>
                 <input type="submit" value="Agregar paciente" class="w-full bg-indigo-600 hover:bg-indigo-700 transition-colors cursor-pointer text-white font-bold p-2 rounded-md">
             </form>
